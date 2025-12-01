bc9fff5132331c22829aab4e51de4770f5a8fc5f
muhammadzhuhry <muhammadzhuhry@gmail.com>
2025-12-01 10:53:34 +0700

    refactor: remove unused Button import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { Button } from "@/components/ui/button"
 import { Hexagon, Menu, X } from "lucide-react"
 import { HeroSection } from "@/components/HeroSection"
 import { FeaturesSection } from "@/components/FeaturesSection"
